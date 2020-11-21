8d45f0fad54d88626dd6855330ef58acafd58823
Yana Petrachenko <petrachenko.iana2011@yandex.ru>
2020-11-21 16:18:37 +0300

    Create default categories

--- app/src/main/java/com/example/cash_you/ui/categories/CategoriesFragment.java
+++ app/src/main/java/com/example/cash_you/ui/categories/CategoriesFragment.java
@@ -27,10 +27,31 @@ public class CategoriesFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = (View) inflater.inflate(R.layout.fragment_categories, container, false);
-        final TextView textView = (TextView) view.findViewById(R.id.text_home);
 
-        Category category = new Category("Food");
-        textView.setText(category.GetName());
+        // Create default categories - Food, Eating out, Fare, Purchases
+        Category foodCategory = new Category("Food");
+        final TextView foodName = (TextView) view.findViewById(R.id.foodName);
+        final TextView foodBalance = (TextView) view.findViewById(R.id.foodBalance);
+        foodName.setText(foodCategory.GetName());
+        foodBalance.setText(foodCategory.GetCurrentBalance());
+
+        Category eatingOutCategory = new Category("Eating out");
+        final TextView eatingOutName = (TextView) view.findViewById(R.id.eatingOutName);
+        final TextView eatingOutBalance = (TextView) view.findViewById(R.id.eatingOutBalance);
+        eatingOutName.setText(eatingOutCategory.GetName());
+        eatingOutBalance.setText(eatingOutCategory.GetCurrentBalance());
+
+        Category fareCategory = new Category("Fare");
+        final TextView fareName = (TextView) view.findViewById(R.id.fareName);
+        final TextView fareBalance = (TextView) view.findViewById(R.id.fareBalance);
+        fareName.setText(fareCategory.GetName());
+        fareBalance.setText(fareCategory.GetCurrentBalance());
+
+        Category purchasesCategory = new Category("Purchases");
+        final TextView purchasesName = (TextView) view.findViewById(R.id.purchasesName);
+        final TextView purchasesBalance = (TextView) view.findViewById(R.id.purchasesBalance);
+        purchasesName.setText(purchasesCategory.GetName());
+        purchasesBalance.setText(purchasesCategory.GetCurrentBalance());
 
         return view;
     }
